c433c408be281494106c539e279b3ccb5444b00d
Simon Gordon <gordonsimon@hotmail.com>
2017-12-11 19:13:44 +1000

    Add bootstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,85 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Jumbotron } from 'react-bootstrap';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  Jumbotron,
+  Navbar,
+  Nav,
+  NavDropdown,
+  MenuItem,
+  NavItem
+} from 'react-bootstrap';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
+import { LinkContainer } from 'react-router-bootstrap';
+
+interface SiteNavProps {}
+const SiteNav = (props: SiteNavProps) => {
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">React-Bootstrap</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <LinkContainer to="/" exact>
+          <NavItem eventKey={1}>Home</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/asdf">
+          <NavItem eventKey={1}>Asdf</NavItem>
+        </LinkContainer>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  );
+};
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={() => (
-              <div className="App container">
-                <Jumbotron>
-                  <div className="App-header">
-                    <h2>Welcome to React</h2>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <div>
+                  <SiteNav />
+                  <div className="container">
+                    <Jumbotron>
+                      <div className="App-header">
+                        <h2>Welcome to React</h2>
+                      </div>
+                      <p className="App-intro">
+                        To get started, edit <code>src/App.tsx</code> and save
+                        to reload.
+                        <Link to="/asdf">Other page</Link>
+                      </p>
+                    </Jumbotron>
                   </div>
-                  <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to
-                    reload.
-                  </p>
-                </Jumbotron>
-              </div>
-            )}
-          />
-          <Route
-            path="/asdf"
-            exact
-            render={() => <div className="App container">asdf</div>}
-          />
-          <Route path="/abc" exact>
-            abcdefg
-          </Route>
-        </Switch>
-      </BrowserRouter>
+                </div>
+              )}
+            />
+            <Route
+              path="/asdf"
+              exact
+              render={() => (
+                <div>
+                  <SiteNav />
+                  <div className="App container">asdf</div>
+                </div>
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
