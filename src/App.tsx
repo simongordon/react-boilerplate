50085d76fa9c7a5a16caff4abcc444730d465240
Simon Gordon <gordonsimon@hotmail.com>
2017-12-11 18:57:07 +1000

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
-import * as React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import { Jumbotron } from "react-bootstrap";
+import * as React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Jumbotron } from 'react-bootstrap';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App container">
-        <Jumbotron>
-          <div className="App-header">
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-          </p>
-        </Jumbotron>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <div className="App container">
+                <Jumbotron>
+                  <div className="App-header">
+                    <h2>Welcome to React</h2>
+                  </div>
+                  <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to
+                    reload.
+                  </p>
+                </Jumbotron>
+              </div>
+            )}
+          />
+          <Route
+            path="/asdf"
+            exact
+            render={() => <div className="App container">asdf</div>}
+          />
+          <Route path="/abc" exact>
+            abcdefg
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
