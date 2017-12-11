419f72b99e28f9ff63436c29934a90d30e7160e1
Simon Gordon <gordonsimon@hotmail.com>
2017-12-11 19:22:34 +1000

    Extract navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import {
-  Jumbotron,
-  Navbar,
-  Nav,
-  NavDropdown,
-  MenuItem,
-  NavItem
-} from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
-import { LinkContainer } from 'react-router-bootstrap';
-
-interface SiteNavProps {}
-const SiteNav = (props: SiteNavProps) => {
-  return (
-    <Navbar>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#">React-Bootstrap</a>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav>
-        <LinkContainer to="/" exact>
-          <NavItem eventKey={1}>Home</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/asdf">
-          <NavItem eventKey={1}>Asdf</NavItem>
-        </LinkContainer>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.4}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-    </Navbar>
-  );
-};
+import CustomNavbar from './components/CustomNavbar';
 
 class App extends React.Component {
   render() {
@@ -51,7 +16,7 @@ class App extends React.Component {
               exact
               render={() => (
                 <div>
-                  <SiteNav />
+                  <CustomNavbar />
                   <div className="container">
                     <Jumbotron>
                       <div className="App-header">
@@ -72,7 +37,7 @@ class App extends React.Component {
               exact
               render={() => (
                 <div>
-                  <SiteNav />
+                  <CustomNavbar />
                   <div className="App container">asdf</div>
                 </div>
               )}
---
+++ src/components/CustomNavbar.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Navbar, Nav, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+
+interface CustomNavbarProps {}
+const CustomNavbar = (props: CustomNavbarProps) => {
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">React-Bootstrap</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <LinkContainer to="/" exact>
+          <NavItem eventKey={1}>Home</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/asdf">
+          <NavItem eventKey={1}>Asdf</NavItem>
+        </LinkContainer>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
