e65eba1bf079bdd6ab894094a35c9c15aadf48ef
Simon Gordon <gordonsimon@hotmail.com>
2017-12-11 18:44:52 +1000

    Add prettier (to vscode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import * as React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Jumbotron } from 'react-bootstrap';
-
+import * as React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Jumbotron } from "react-bootstrap";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App container">
         <Jumbotron>
-
           <div className="App-header">
             <h2>Welcome to React</h2>
           </div>
           <p className="App-intro">
             To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          </p>
         </Jumbotron>
       </div>
     );
